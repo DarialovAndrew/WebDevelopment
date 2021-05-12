f0cc5b37834a8d1901f114c37665dc0c4a042ebd
dimdbr <ddd260501@gmail.com>
2021-05-12 17:51:02 +0300

    password visibility correct toggling

--- scripts/toggle_psw_visibility.js
+++ scripts/toggle_psw_visibility.js
@@ -4,17 +4,17 @@ function switch_visibility() {
     var eye = document.getElementById("togglePsw");
     if (password.type === "password") {
         password.type = "text";
-        if (password2 != null) {
+        if (document.body.contains(password2)) {
             password2.type = "text";
             eye.classList.toggle('fa-eye-slash');
         } else {
             eye.classList.toggle('fa-eye-slash');
         }
     } else {
-        if (password2 != null) {
+        if (document.body.contains(password2)) {
             password.type = "password";
             password2.type = "password";
-            eye.classList.toggle('fa-eye-slash');
+
         }
         password.type = "password";
         eye.classList.toggle('fa-eye-slash');
